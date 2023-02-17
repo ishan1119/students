a0305f534955281b4869a1cce57b3f37cacf48f9
arturogonzalezm <arturo@arturosolutions.com.au>
2023-02-17 16:16:38 +1100

    Added comments

--- src/employee.py
+++ src/employee.py
@@ -5,9 +5,19 @@ import pandas as pd
 
 def read_employee_data(data):
     """
+    Reads employee data from a CSV file and returns a pandas DataFrame.
 
-    :param file:
-    :return:
+    filepath_or_buffer: A string representing the file path or a file-like object of the CSV data.
+    quotechar: A character used to denote quotes in the CSV file.
+    header: An integer or list of integers representing the row number(s) to use as the column names.
+    sep: A regular expression pattern used to separate fields in the CSV file.
+    skipinitialspace: A boolean value indicating whether to skip leading spaces in the fields.
+    quoting: An integer constant representing the CSV quoting style.
+    engine: The parsing engine to use.
+    After reading the CSV data, we replace any spaces in the column names with underscores using the str.replace method on the columns attribute of the DataFrame.
+
+    :param data: A string representing the file path or a file-like object of the CSV data.
+    :return: A pandas DataFrame containing the employee data.
     """
     employee_df = pd.read_csv(filepath_or_buffer=data, quotechar='"', header=0, sep=',\s*', skipinitialspace=True,
                               quoting=csv.QUOTE_ALL, engine='python')
